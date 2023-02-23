b13f1cb217d6e09f95cbe794318871e682f26c74
Elva Yudista <yudistaelva@gmail.com>
2023-02-23 15:19:52 +0700

    Create About us and Shopping

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,14 @@
-// Toggle class active
+// Transition
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const href = link.getAttribute('href');
+      const offsetTop = document.querySelector(href).offsetTop;
+  
+      scroll({
+        top: offsetTop,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
